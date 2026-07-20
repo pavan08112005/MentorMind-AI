8b79c0896c9bc0307e728393f7ed0b5beed2cf12
pavan08112005 <pavankumar462104@gmail.com>
2026-07-20 16:56:58 +0530

    Updated README with project description

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Welcome to MentorMind AI 🚀</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    @app.route("/login")
+    def login():
+    return "Login Page"
