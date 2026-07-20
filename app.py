9cc943150d7dfdc833538cff3f36f18d8e81c93c
pavan08112005 <pavankumar462104@gmail.com>
2026-07-21 02:18:41 +0530

    Created 1st HTML template and css

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to MentorMind AI 🚀</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    @app.route("/login")
-    def login():
-    return "Login Page"
